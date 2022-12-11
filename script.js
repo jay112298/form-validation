e392512f42df2c46b26aec472a05679ab2e6464d
Jay Asole <jasole1111@gmail.com>
2022-12-11 09:11:08 +0530

    Add name USN object and namefield validation

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function validateName() {
     var nameWarn = document.getElementById('name-warn')
 
     if (nameValue === undefined || nameValue == "") {
+        nameField.classList.add('input-red')
         nameWarn.style.display = "block"
         nameWarn.innerHTML = "Please enter your name"
 
@@ -12,6 +13,7 @@ function validateName() {
         nameWarn.innerHTML = "Name should be at least two characters long"
     }
      else {
+        nameField.classList.remove('input-red')
         nameWarn.style.display = "none"
     }
 }
@@ -21,15 +23,60 @@ function validateMail() {
     var emailValue = emailField.value
     var emailWarn = document.getElementById('email-warn')
 
-    if (emailValue === undefined || nameValue == "") {
+    if (emailValue === undefined || emailValue == "") {
+        emailField.classList.add('input-red')
         emailWarn.style.display = "block"
         emailWarn.innerHTML = "Please enter your email"
 
-    } else if (nameValue.length < 2) {
+    } else if (emailValue.includes("@")) {
+        emailField.classList.add('input-red')
         emailWarn.style.display = "block"
-        emailWarn.innerHTML = "Name should be at least two characters long"
+        emailWarn.innerHTML = "Email should be valid"
     }
      else {
+        nameField.classList.remove('input-red')
         emailWarn.style.display = "none"
+    }
+}
+
+function validateGender() {
+    var genderField = document.getElementById('gender-inp');
+    var genderValue = genderField.value
+    var genderWarn = document.getElementById('gender-warn')
+
+    if (genderValue === undefined || genderValue == "") {
+        genderField.classList.add('input-red')
+        genderWarn.style.display = "block"
+        genderWarn.innerHTML = "Please enter your email"
+
+    } else if (genderValue.includes("@")) {
+        genderField.classList.add('input-red')
+        genderWarn.style.display = "block"
+        genderWarn.innerHTML = "Email should be valid"
+    }
+     else {
+        nameField.classList.remove('input-red')
+        genderWarn.style.display = "none"
+    }
+}
+
+function validatePass() {
+    var passField = document.getElementById('pass-inp');
+    var passValue = emailField.value
+    var passWarn = document.getElementById('pass-warn')
+
+    if (emailValue === undefined || emailValue == "") {
+        passField.classList.add('input-red')
+        passWarn.style.display = "block"
+        passWarn.innerHTML = "Please enter your password"
+
+    } else if (emailValue.includes("@")) {
+        pass.classList.add('input-red')
+        pass.style.display = "block"
+        pass.innerHTML = "Password should be atleast 8 characters long"
+    }
+     else {
+        passField.classList.remove('input-red')
+        passWarn.style.display = "none"
     }
 }
